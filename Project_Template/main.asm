a8ffd1c49bcfd17e3e64bad0fcaa50f05000c7e8
Mina Toma <minatoma1999@gmail.com>
2019-12-05 20:02:59 +0200

    Constructing Occurrence Array

--- Project_Template/main.asm
+++ Project_Template/main.asm
@@ -2,28 +2,96 @@
 INCLUDE Irvine32.inc
 .DATA
 
-COMMENT @
-	This is the main container that simulates the original prioiry queue
-	Data will be like
-	fistBit secondBit thirdBit fourthBit
-	------Occurrences--------- --value--
-@
-priorityQueue dword 300 dup(0)
+;--------------------------------------------------------------------------
+;	This is the main container that simulates the original prioiry queue
+;	Data will be like
+;	FirstDword			SecondDword			ThirdDword
+;	Occurrences			Value				Index in huffmanTree
+;--------------------------------------------------------------------------
+priorityQueue dword 1024 dup(-1)
 
-;===========================================================================
 
-COMMENT @
-	The size of priorityQueue
-@
-Size
+;--------------------------------------------------------------------------
+;The size of priorityQueue
+;--------------------------------------------------------------------------
+priorityQueueSize dword 0
 
+
+;--------------------------------------------------------------------------
+;	The main data structure to hold huffman tree nodes
+;	Data will be like
+;	FirstDword			SecondDword			ThirdDword
+;	Value				LeftChild			RightChild
+;--------------------------------------------------------------------------
+huffmanTree dword 2048 dup(-1)
+
+;--------------------------------------------------------------------------
+;	The size of huffmanTree
+;--------------------------------------------------------------------------
+huffmanTreeSize dword 0
+
+;--------------------------------------------------------------------------
+; string for testing
+;--------------------------------------------------------------------------
+testingString byte "howaree";
 
 
 .code
 main PROC
-
+	
+	CALL constructOccurrencesArray
 
 	exit
 main ENDP
 
+constructOccurrencesArray PROC
+	
+	mov ecx, lengthof testingString
+	mov esi, offset testingString
+	mov edi, offset priorityQueue
+
+	loopOverInputSting:
+		push ecx
+		
+		movzx eax, byte ptr [esi]
+		mov ecx, priorityQueueSize
+		push esi
+		mov esi, offset priorityQueue
+		
+		cmp ecx, 0
+		jz emptyPriorityQueue
+		loopOverOccerrenceArray:
+			cmp [esi + type priorityQueue], eax
+			jnz continue		
+			inc dword ptr [esi]
+			jmp foundOccurrence
+
+			continue:
+			add esi, type priorityQueue
+			add esi, type priorityQueue
+			add esi, type priorityQueue
+		loop loopOverOccerrenceArray
+
+		emptyPriorityQueue:
+
+		mov dword ptr [esi], 1
+		mov [esi + type priorityQueue], eax
+
+		inc priorityQueueSize
+
+		foundOccurrence:
+		pop esi
+		pop ecx
+		inc esi
+	loop loopOverInputSting
+
+	mov eax, priorityQueueSize
+	call writedec
+
+	RET
+constructOccurrencesArray ENDP
+
 END main
+
+
+
