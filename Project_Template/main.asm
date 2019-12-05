acbf57a68d63bf399f7b9dbc767d9239ddea64c6
Mina Toma <minatoma1999@gmail.com>
2019-12-05 18:37:54 +0200

    Removind redundant files

--- Project_Template/main.asm
+++ Project_Template/main.asm
@@ -1,11 +1,27 @@
 
 INCLUDE Irvine32.inc
 .DATA
+
+COMMENT @
+	This is the main container that simulates the original prioiry queue
+	Data will be like
+	fistBit secondBit thirdBit fourthBit
+	------Occurrences--------- --value--
+@
+priorityQueue dword 300 dup(0)
+
+;===========================================================================
+
+COMMENT @
+	The size of priorityQueue
+@
+Size
+
+
+
 .code
 main PROC
 
-	mov eax, 3
-	call writedec
 
 	exit
 main ENDP
