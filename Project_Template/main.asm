b6bf152f727b4248ef8ea61b5df5ac772a3fba91
QOTB912 <mahmoudqotb912@gmail.com>
2019-12-07 02:03:21 +0200

    Finished two funs to read (Decompression or Compression) files

--- Project_Template/main.asm
+++ Project_Template/main.asm
@@ -1,4 +1,6 @@
 INCLUDE Irvine32.inc
+INCLUDE macros.inc
+BUFFER_SIZE = 5000
 .DATA
 
 
@@ -49,17 +51,204 @@ shiftOffset dword 12
 ;--------------------------------------------------------------------------
 maxValue dword 1000000000
 
+
+
+;--------------------------------------------------------------------------
+; TODO write disc readAllFile
+;--------------------------------------------------------------------------
+
+
+buffer BYTE BUFFER_SIZE DUP(?)
+filename BYTE 80 DUP(0)
+fileSize DWORD ?
+fileHandle HANDLE ?
+numberOfChar DWORD ?
+;--------------------------------------------------------------------------
+; TODO write disc readCompressionFile
+;compression
+;--------------------------------------------------------------------------
+alphabe byte 2000 dup(?)
+alphabetLenght dword 0
+
+tree DWORD 2000 dup(?)
+treeLenght dword 0
+
+nodeCount DWORD -1
+nodes DWORD 2000 dup(?)
+nodesOffset DWORD ?
+nodesOffsetCount DWORD ?
+ecx_temp1 DWORD ?
+zero DWORD 0
+
+;--------------------------------------------------------------------------
+; TODO write disc readheader
+;compression
+;--------------------------------------------------------------------------
+_space byte 32
+_endl1 byte 13
+_endl2 byte 10
+_countendl word 0
+_one word 1
+_countCharLenght DWORD 0
+
+;--------------------------------------------------------------------------
+; TODO write disc readdDecompressionFile
+;--------------------------------------------------------------------------
+
 ;==========================================================================
 ;==========================================================================
 ;==========================================================================
 
+
+arr byte 4 dup(?)
+arr2 byte "123456789",0
+arr3 byte 1,2,3,4,5,6,7,8,9
+nu dword ?
+test1 byte ?
 
 .code
 main PROC
 	
-	CALL constructOccurPQ
-	CALL constructTree
+	;CALL constructOccurPQ
+	;CALL constructTree
 
+	;C:\Users\Mahmo\Desktop\prog_3\ass\test.tx
+	;call readAllFile
+
+	;call readCompressionFile 
+	;mov edx , ecx
+	;call writeString
+	;call crlf
+
+
+	call readDecompressionFile
+
+	mov ecx , treeLenght
+
+	mov edx , offset tree
+	lll:
+	mov  eax , [edx]
+	add edx , 4
+	call writedec
+	call crlf
+	LOOP lll
+
+	mov ecx , alphabetlenght
+	mov edx , offset alphabe
+	ll2:
+	mov al , [edx]
+	call writechar
+	inc edx
+	LOOP ll2
+
+
+	mov ecx , nodeCount
+	inc ecx
+	;call readint
+	mov edx , offset nodes
+	ll3:
+	mov  eax , [edx]
+	add edx , 4
+	call writedec
+	call crlf
+	loop ll3
+			;nodeCount	10	unsigned long
+
+	
+	;mov ecx , fileSize
+	;mov al  , 'w'
+	;mov ah , 19
+	;cmp al , ah
+	;je testt
+
+	;mov eax , 0
+	;l1:
+	;mov al , [edx]
+	;call writechar
+	;inc edx
+	;loop l1
+	;testt:
+	;mov eax , 5
+	;call readdec
+
+
+	mov bl ,1
+	movzx ebx , bl
+
+
+	mov ecx , 9
+LL:
+	mov edx , offset arr2
+	;shl eax,4
+	;call writedec
+	;mov nu , eax
+	;rol eax , 16
+	mov  eax , dword ptr  [edx]
+	and al , 11001111b
+	and ah , 11001111b
+	;or eax , 00110000b
+	inc edx
+	LOOP LL
+	call dumpRegs
+
+
+	mov ecx , 4
+	mov edx , offset arr
+	lx:
+	call readchar
+	call writechar
+	mov [edx] , al
+	inc edx
+	
+
+	LOOP lx
+
+	mov edx , offset arr2
+
+
+
+	mov bl , 2
+	mov dl , 2
+
+	mov eax , 0
+	or al , bl
+	shl eax , 4
+	or al , dl
+	shl eax , 4
+
+
+	mov ecx , 3
+	mov edx , offset  arr
+	mov al , 1
+	mov ah , 2
+	
+
+	mov eax , 0
+	l3:
+	mov bl , 	_space
+	cmp [edx] , bl
+	jne not_space
+	;jmp  
+	not_space:
+		or al , [edx]
+		shl eax , 4
+		jmp l3
+
+
+
+	
+	
+	mWrite <"yes",0dh,0ah>	
+	l2:
+		call readchar
+		call writechar
+		mov [edx] , al
+		inc edx
+	loop l2
+	mov eax , offset  arr
+	call writedec
+	mov  eax , offset  arr
+	call writedec
 	exit
 main ENDP
 
@@ -241,5 +430,231 @@ reSortPQ PROC
 
 	RET
 reSortPQ ENDP
+
+
+
+;--------------------------------------------------------------------------
+; TODO write disc
+;--------------------------------------------------------------------------
+
+readAllFile PROC
+
+	; Let user input a filename.
+	mWrite "Enter an input filename: "
+	mov edx,OFFSET filename
+	mov ecx,SIZEOF filename
+	call ReadString		
+
+
+	; Open the file for input.
+	mov edx,OFFSET filename
+	call OpenInputFile
+	mov fileHandle,eax
+	
+	
+	; Check for errors.
+	cmp eax,INVALID_HANDLE_VALUE			 ; error opening file?
+	jne file_ok								 ; no: skip
+	mWrite <"Cannot open file",0dh,0ah>
+	jmp quit
+
+
+
+	; Read the file into a buffer.
+	file_ok:
+		mov edx,OFFSET buffer
+		mov ecx,BUFFER_SIZE
+		call ReadFromFile
+		jnc check_buffer_size
+		mWrite "Error reading file. "
+		call WriteWindowsMsg
+		jmp close_file
+
+
+		check_buffer_size:
+			cmp eax,BUFFER_SIZE
+			jb buf_size_ok
+			mWrite <"Error: Buffer too small for the file",0dh,0ah>
+			jmp quit
+
+		buf_size_ok:
+			
+			mov buffer[eax],0
+			mWrite "File size: "
+			call WriteDec
+			mov fileSize , eax
+			call Crlf
+			;To Display
+			mWrite <"Buffer:",0dh,0ah,0dh,0ah>					;TODO SER 0dh,0ah,0dh,0ah
+			mov edx,OFFSET buffer
+			call WriteString
+			call Crlf
+
+		close_file:
+			mov eax,fileHandle
+			call CloseFile
+		quit:
+			RET
+readAllFile ENDP
+
+
+
+
+;--------------------------------------------------------------------------
+; TODO write disc
+;--------------------------------------------------------------------------
+
+
+readCompressionFile PROC
+
+
+	call readAllFile
+	;TODO remove this line (they use the offser of buffer) || creating new string
+	mov ecx,OFFSET buffer
+	
+
+	RET
+readCompressionFile ENDP
+
+
+;--------------------------------------------------------------------------
+; TODO write disc
+;--------------------------------------------------------------------------
+readDecompressionFile PROC
+	
+
+	call readAllFile
+	mov ebx , fileSize
+	mov numberOfChar , ebx
+	;sub numberOfChar , 4
+	mov eax , 0
+	mov edx , OFFSET buffer
+	mov ecx , OFFSET nodes
+	;mov nodesOffset , ecx
+	mov nodesOffsetCount , ecx 
+	mov ecx , OFFSET tree
+	l3:
+		mov bl , 	_endl1
+		cmp [edx] , bl
+		je new_Line
+		;cmp _countCharLenght , numberOfChar                ;TODO handel this test case
+		;je end_readAlphabet
+		mov bl , 	_space
+		cmp [edx] , bl
+		jne add_to_tree      
+		inc edx
+		inc _countCharLenght
+
+
+		mov   [ecx] , eax
+		add  ecx  , 4
+		add treeLenght , 1
+		cmp eax , zero
+		je skip
+		inc nodeCount
+		push ecx
+		mov ecx , nodesOffsetCount 
+		mov [ecx] , eax
+		add ecx , 4
+		mov nodesOffsetCount , ecx
+		pop ecx
+
+
+		;call writedec						; TODO push eax on the tree
+		;call crlf
+
+		skip:
+			mov eax , 0
+			jmp l3
+		new_Line:
+			sub numberOfChar , 2
+			inc _countendl
+			add edx , 2
+			
+			;sub ecx , 2
+			mov bx, _one
+			cmp _countendl , bx
+			jne end_ReadDecompressionFile
+			inc treeLenght
+			mov ebx , treeLenght
+			push ebx
+			
+
+			call readAlphabet
+			pop ebx 
+			mov treeLenght , ebx
+
+
+			jmp end_ReadDecompressionFile
+
+		add_to_tree:                   
+			
+			
+			mov  bl ,  [edx]
+			and bl , 11001111b
+			movzx ebx , bl
+			push edx
+			push ebx
+			mov edx , 0
+			mov ebx , 10
+			mul ebx
+			pop ebx                    ; TODO check cf
+			add eax , ebx 
+			pop edx
+
+			;or al , [edx]
+			;shl eax , 4
+			inc edx 
+			inc _countCharLenght
+			jmp l3
+
+	
+
+
+	end_ReadDecompressionFile:
+		RET
+readDecompressionFile ENDP
+
+
+;--------------------------------------------------------------------------
+; TODO write disc readAlphabet + save reg
+;--------------------------------------------------------------------------
+
+
+readAlphabet PROC
+
+	
+	mov ecx , OFFSET alphabe
+	l5:
+		mov bl , [edx]
+		mov test1 , bl
+		mov bl , 	_endl1
+		cmp [edx] , bl
+		je end_readAlphabet
+		mov ebx , numberOfChar
+		;add ebx , 1
+		cmp _countCharLenght , ebx
+		jae end_readAlphabet
+		mov bl , 	_space
+		cmp [edx] , bl
+		jne  add_Alphabet
+		inc edx
+		inc _countCharLenght
+		jmp l5
+		add_Alphabet:
+			mov bl ,   [edx] 
+			mov [ecx] , bl 
+
+			inc edx
+			inc ecx
+			inc _countCharLenght
+			inc alphabetLenght
+			jmp l5
+	
+	end_readAlphabet:
+		sub numberOfChar , 2
+		RET
+readAlphabet ENDP
+
 
 END main
